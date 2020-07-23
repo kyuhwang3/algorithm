a58f50012f55c80e694f81f10f9c95cc966ab152
Kyu Hwang Kim <kyuhwang.kim@lge.com>
2020-07-23 13:25:17 +0900

    2787 directory commit

---
+++ 2798/2798_kk.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int m, n;
+vector<int> deck;
+void doProcess(){
+    do {
+
+    } while (next_permutation)
+}
+
+int main(){
+
+    return 0;
+}
